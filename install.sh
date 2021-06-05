81417af7b6bcf9a2987867a7c4c4f2d5055298b5
artem59k <akartasov23@gmail.com>
2021-06-05 15:48:16 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #Установщик скриптов
+echo " installing BashScriptPakFFW."
 cp scripts/tt5-set /usr/local/bin
 cp scripts/usersystemd-set /usr/local/bin
 chmod +x /usr/local/bin/tt5-set
 chmod +x /usr/local/bin/usersystemd-set
+echo "Installation compleat
