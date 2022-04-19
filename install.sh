7cfabcf96d2bb01500b1454d95b0627c22574841
artem59k <78084126+artem59k@users.noreply.github.com>
2022-04-19 15:45:38 +0500

    Complete code refactoring. Updating some scripts

--- install.sh
+++ install.sh
@@ -1,21 +1,5 @@
 #Установщик скриптов
-echo " installing BashScriptPakFFW."
-cp scripts/tt5-set /usr/local/bin
-cp scripts/usersystemd-set /usr/local/bin
-cp scripts/ttbot-install /usr/local/bin
-cp scripts/system-timer /usr/local/bin
-cp scripts/ttbot-config /usr/local/bin
-cp scripts/botdocker-start /usr/local/bin
-chmod +x /usr/local/bin/botdocker-start
-chmod +x /usr/local/bin/ttbot-config
-chmod +x /usr/local/bin/tt5-set
-chmod +x /usr/local/bin/usersystemd-set
-chmod +x /usr/local/bin/ttbot-install
-chmod +x /usr/local/bin/system-timer
-cp scripts/site-startup /usr/local/bin
-cp scripts/ftp-user /usr/local/bin
-chmod +x /usr/local/bin/site-startup
-chmod +x /usr/local/bin/ftp-user
-cp scripts/updater /usr/local/bin
-chmod +x /usr/local/bin/updater
-echo "Installation compleat."
+echo "установка BashScriptPakFFW."
+cp scripts/* /usr/local/bin
+chmod -R +x /usr/local/bin
+echo "Установка завершена."
